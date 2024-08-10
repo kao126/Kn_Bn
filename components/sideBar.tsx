7e370d7fd1f8e3d29e356ad20ae6c56340470acd
Kanaru Oshima <kgiy.007ruu@gmail.com>
2024-08-10 23:26:35 +0900

    update: change colors by clicking side-bar lists

--- components/sideBar.tsx
+++ components/sideBar.tsx
@@ -6,7 +6,43 @@ import { Users } from './icons/users';
 import Link from 'next/link';
 
 export function SideBar() {
-  const pathname = usePathname();
+  const handleComponent = (listName: string) => {
+    const pathname = usePathname().replace('/', '');
+    const color = listName == pathname ? '#bf94ff' : '#94a3b8';
+
+    const switchComponent = (listName: string) => {
+      switch (listName) {
+        case 'users':
+          return <Users color={color} className="mx-auto" />;
+        case 'task_board':
+          return <Task color={color} className="mx-auto" />;
+        case 'settings':
+          return <Setting color={color} className="mx-auto" />;
+      }
+    };
+
+    const changeCase = (listName: string): string => {
+      return listName
+        .split('_')
+        .map((word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
+        .join(' ');
+    };
+
+    return (
+      <Link href={listName}>
+        <li className="relative h-full border-b-2 flex flex-col justify-center items-center p-2">
+          {listName == pathname ? (
+            <span
+              className="block absolute -inset-0 -left-0 w-1 bg-[#bf94ff]"
+              aria-hidden="true"
+            ></span>
+          ) : null}
+          {switchComponent(listName)}
+          <span className={`text-[${color}]`}>{changeCase(listName)}</span>
+        </li>
+      </Link>
+    );
+  };
 
   return (
     <div className="grid grid-rows-[72px_auto] text-center bg-white border-r-slate-600">
@@ -14,43 +50,10 @@ export function SideBar() {
         Kn_Bn
       </h1>
       <ul className="grid grid-cols-1 grid-rows-[125px_125px_auto_125px]">
-        <Link href="users">
-          <li className="relative h-full border-b-2 flex flex-col justify-center items-center p-2">
-            {pathname == '/users' ? (
-              <span
-                className="block absolute -inset-0 -left-0 w-1 bg-[#bf94ff]"
-                aria-hidden="true"
-              ></span>
-            ) : null}
-            <Users className="mx-auto" />
-            <span className="text-[#94a3b8]">Users</span>
-          </li>
-        </Link>
-        <Link href="task_board">
-          <li className="relative h-full border-b-2 flex flex-col justify-center items-center p-2">
-            {pathname == '/task_board' ? (
-              <span
-                className="block absolute -inset-0 -left-0 w-1 bg-[#bf94ff]"
-                aria-hidden="true"
-              ></span>
-            ) : null}
-            <Task className="mx-auto" />
-            <span className="text-[#bf94ff] text-bold">Task Board</span>
-          </li>
-        </Link>
+        {handleComponent('users')}
+        {handleComponent('task_board')}
         <li className="h-full border-b-2 p-2"></li>
-        <Link href="settings">
-          <li className="relative h-full flex flex-col justify-center items-center p-2">
-            {pathname == '/settings' ? (
-              <span
-                className="block absolute -inset-0 -left-0 w-1 bg-[#bf94ff]"
-                aria-hidden="true"
-              ></span>
-            ) : null}
-            <Setting className="mx-auto" />
-            <span>Setting</span>
-          </li>
-        </Link>
+        {handleComponent('settings')}
       </ul>
     </div>
   );
